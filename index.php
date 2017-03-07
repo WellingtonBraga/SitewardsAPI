46d638d7a58b361cd080d014cc288a40a8256f8b
WellingtonBraga <wellington.jbraga@live.com>
2017-03-07 19:50:41 -0300

    Including some comments in index.php and pass displayErroDetails to false.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $bookController->initializeDatabase();
 
 $configuration = [
     'settings' => [
-        'displayErrorDetails' => true,
+        'displayErrorDetails' => false,
     ],
 ];
 
@@ -18,18 +18,25 @@ $container = new \Slim\Container($configuration);
 
 $app = new App($container);
 
+// Return the full book's list.
 $app->get('/books', BooksController::class . ':listAllBooks');
 
+// Return only one book based in its name
 $app->get('/books/name/{name}', BooksController::class . ':findByName');
 
+// Return all books from an specified author
 $app->get('/books/author/{author}', BooksController::class . ':findByAuthor');
 
+// Return all books which are with an specified holder (who belongs it now)
 $app->get('/books/holder/{holder}', BooksController::class . ':findByHolder');
 
+// Return only one book based in its id
 $app->delete('/books/{id}', BooksController::class . ':delete');
 
+// Add a new book
 $app->post('/books', BooksController::class . ':add');
 
+// Change the holder of an specified book
 $app->put('/books/{id}', BooksController::class . ':changeHolder');
 
 $app->run();
